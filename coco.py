5edd8da92e8552cd73cdc04d4c81a741b5df5dfc
AnonymousSubmissions <52239830+AnonymousSubmissions@users.noreply.github.com>
2019-06-26 14:03:30 +0400

    Update coco.py

--- coco.py
+++ coco.py
@@ -1,11 +1,3 @@
-"""VOC Dataset Classes
-
-Original author: Francisco Massa
-https://github.com/fmassa/vision/blob/voc_dataset/torchvision/datasets/voc.py
-
-Updated by: Ellis Brown, Max deGroot
-"""
-
 import os
 import pickle
 import os.path
